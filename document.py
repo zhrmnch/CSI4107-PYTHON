2c9623d0517856c831677c9b9b2ffe161faae0ba
zhrmnch <60791214+zhrmnch@users.noreply.github.com>
2023-02-09 15:19:02 -0500

    ver 2

--- document.py
+++ document.py
@@ -2,6 +2,7 @@ import nltk
 from pathlib import Path
 import re
 from nltk.tokenize import word_tokenize
+from nltk.stem import *
 
 def readAllFile(path):
     '''
@@ -36,9 +37,9 @@ def retrievingStopwords():
             stopwordArray.append((line.split())[0])
     return stopwordArray
 
-def removeStopWord(corpus):
+def removeStopWord(dict):
     '''
-    Removes stopwords within corpus and html tags
+    Removes stopwords in the values of the dictionary, we are updating the original dictionary
 
     @param: corpusToken: all tokens in the given corpus
     @param: stopwordsToken: list of all stop words
@@ -47,10 +48,14 @@ def removeStopWord(corpus):
     @return: string without stopwords and tags 
     '''
 
-    corpusToken = word_tokenize(corpus)
-    stopwordsToken=  retrievingStopwords()
-    tokensWithoutStopwords = [word for word in corpusToken if not word in stopwordsToken]
-    return tokensWithoutStopwords
+    stopwordsToken = retrievingStopwords()
+
+    stemmer = PorterStemmer()
+
+    for k,v in dict.items():
+        dict[k] = word_tokenize(v)
+        dict[k] = [stemmer.stem(i) for i in dict[k]]
+        dict[k] = [word for word in dict[k] if not word in stopwordsToken]
 
 
 def documentParser(corpus):
@@ -70,13 +75,43 @@ def documentParser(corpus):
         for match in tmpdoc:
             docList = re.findall(dividerRegex, match[0], flags=re.DOTALL)
             if docList:
-                #to remove punctuationa and markups
-                #TODO re-think about numbers
-                dict[docList[0][0]] = re.sub("[^\w\s]"," ",re.sub("<[^>]*>", "",docList[0][1]))
+                #to remove punctuationa and markups and numbers
+                dict[docList[0][0]] = re.sub("\d+","",re.sub("[^\w\s]|_"," ",re.sub("<[^>]*>", "",docList[0][1])))
 
     else:
         print("No match found.")
 
+    return dict
+
+def calculateF(dict):
+    '''
+    calculate the f value ; frequency of term i in document j
+    @param dict:
+    @return: dictiory with f values
+    '''
+
+
+    #make dictionary of all words and their occurance over the corpus vocab = {word:{ df: # , docf: { docno: tf}}}
+    vocab = {}
+    for k,v in dict.items():
+        max = 0
+        for word in v:
+            if not word in vocab.keys():
+                vocab[word] = {'docf':{}}
+                vocab[word]['df'] = 1
+                vocab[word]['docf'][k] = 1
+            else:
+                vocab[word]['df'] += 1
+                if k not in vocab[word]['docf'].keys():
+                    vocab[word]['docf'][k] = 1
+                else:
+                    vocab[word]['docf'][k] += 1
+            if vocab[word]['docf'][k] > max:
+                max = vocab[word]['docf'][k]
+        for word in vocab.keys():
+            if k in vocab[word]['docf'].keys():
+                vocab[word]['docf'][k] = vocab[word]['docf'][k] / max
+    #TODO df to idf
 
 if __name__ == '__main__':
 
@@ -97,4 +132,7 @@ if __name__ == '__main__':
     with open("bow.txt", "w") as textFile2:
         for item in vocabulary:
             textFile2.write(item+'\n')
-    
+
+
+
+
