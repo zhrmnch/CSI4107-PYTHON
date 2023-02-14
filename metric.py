579219db89b9ce7e541d92457794cb37bf3de1e4
Othmane Ayoub <oayou029@uottawa.ca>
2023-02-13 21:15:55 -0500

    Adding metric lib

---
+++ metric.py
@@ -0,0 +1,12 @@
+
+def similarity_measure():
+    return
+
+def dot_product(query, document):
+    '''
+
+    @param query: The query vector
+    @param document: The document vector
+    @return:
+    '''
+
